efdd0b3d79185be6375cae807c52f69a10f3614f
oejaramillo <oscar.10000@hotmail.com>
2023-04-11 17:01:10 -0300

    avance

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,15 +3,20 @@ import random
 
 class Hat:
     def __init__(self, **kwargs):
-        if kwargs == None:
-            self = 1
+        if not kwargs or sum(kwargs.values()) < 1:
+            contents = ["color"]
         else:
             self.contents = kwargs
+            
+
+    def __str__(self):
+        return f'{self.contents}'
     
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     print(hat)
 
-hat1 = Hat()
-print(hat1)
+hat1 = Hat(blue=1, red=50)
+hat2 = Hat(blue=0)
+print(hat2)

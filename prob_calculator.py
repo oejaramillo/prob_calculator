dbe4178e3472deeb791ad972ac5185312404dc7a
oejaramillo <oscar.10000@hotmail.com>
2023-04-21 16:43:55 -0300

    avance función

--- prob_calculator.py
+++ prob_calculator.py
@@ -37,22 +37,43 @@ class Hat:
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     contents = hat.contents
 
+    expected_colors = list(expected_balls.keys())
     expected = []
     for key, value in expected_balls.items():
         for x in range(0, value):
            expected.append(key)
 
-    result = hat.draw(num_balls_drawn)
-    if result == expected:
-        proof = 1
-    else:
-        proof = 0
+    total = []
+    realizaciones = 0
+    for y in range(0, num_experiments):
+        result = hat.draw(num_balls_drawn)
+        
+        for x in expected_colors:
+            total.append(result.count(expected_colors))
+
+            if sum(total) >= len(expected):
+                realizacion = 1
+                realizaciones += realizacion
+            else:
+                realizacion = 0
+                realizaciones += realizacion
+                
+
+    
+
+    return f'resultado {result} esperado {expected}, realizaciones {realizaciones}, matriz {total}'
 
 
-    return f'resultados {result} esperado {expected} prueba {proof}'
+hat = Hat(black=6, red=4, green=3)
 
-hat1 = Hat(yellow=3, blue=2, green=6)
+probability = experiment(hat=hat,
+                  expected_balls={"red":2},
+                  num_balls_drawn=5,
+                  num_experiments=5)
 
+print(probability)
 
+resultado = ['black', 'black', 'green', 'red', 'red'] 
+esperado = ['red', 'red', 'green']
 
-print(experiment(hat1, {"yellow":2}, 2, 2000))
+print(esperado in resultado)

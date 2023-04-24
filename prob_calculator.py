896979dd34a6b3a09c26ca0b585062ba8568e6c9
oejaramillo <oscar.10000@hotmail.com>
2023-04-24 16:51:47 -0300

    nuevo intento

--- prob_calculator.py
+++ prob_calculator.py
@@ -35,32 +35,6 @@ class Hat:
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    contents = hat.arguments
-
-    draw = hat.draw(num_balls_drawn)
-    draw_names = list(set(draw)) 
-    draw_counts = []
-    for x in range(0, len(draw_names)):
-        counts = draw.count(draw_names[x])
-        draw_counts.append(counts)
-
-    draw_dic = {item: draw_counts[i] for i, item in enumerate(draw_names)}
-
-    # Comprobamos si expected_balls está en draw_dic
-    total = []
-    expected_balls_names = list(expected_balls.keys())
-
-    # lista con el conteo de las bolas de expected en draw
-    for x in range(0, len(expected_balls_names)):
-        total.append(draw_dic.get(expected_balls_names[x]))
-
-    expected_balls_values = list(expected_balls.values())
-
-    # Comparamos ambas listas valor a valor
-    try:
-        realizaciones = [expected_balls_values[x] <= total[x] for x in range(len(total))]
-    except:
-        realizaciones = False
 
 
         

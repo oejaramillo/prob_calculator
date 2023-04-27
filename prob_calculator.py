872580c9efac6ce79c8222ca64cf1cb9a5a4b036
oejaramillo <oscar.10000@hotmail.com>
2023-04-27 13:15:12 -0300

    falta arreglar .contents

--- prob_calculator.py
+++ prob_calculator.py
@@ -31,38 +31,40 @@ class Hat:
         new = copy.copy(initial)
         new.pop(item)
         initial = new
-    
-
-
+        
       return draw
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
   total = 0
   for x in range(num_experiments):
     dr = hat.draw(num_balls_drawn)
-    if num_balls_drawn < sum(list(expected_balls.values())):
-      total += 0
+    dr_names = list(set(dr))
+    dr_counts = []
+
+    for y in range(len(dr_names)):
+      counts = dr.count(dr_names[y])
+      dr_counts += [counts]
+
+      dr_dic = {key: value for key, value in zip(dr_names, dr_counts)}
+
+    if all(dr_dic.get(key, 0) >= value for key, value in expected_balls.items()):
+      total += 1
     else:
-      dr_names = list(set(dr))
-      dr_counts = []
-
-      for y in range(len(dr_names)):
-        counts = dr.count(dr_names[y])
-        dr_counts += [counts]
-
-        dr_dic = {key: value for key, value in zip(dr_names, dr_counts)}
-
-        if all(dr_dic.get(key, 0) >= value for key, value in expected_balls.items()):
-          total += 1
-        else:
-          total += 0
+      total += 0
   
   prob = total/num_experiments
-  return prob
+  return prob 
 
 hat = Hat(blue=3,red=2,green=6)
-probability = experiment(hat=hat, 
-                         expected_balls={"blue":2,"green":1}, 
-                         num_balls_drawn=4, 
-                         num_experiments=1000)
-print(probability)
+probability = experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
+actual = probability
+expected = 0.272
+
+
+hat2 = Hat(yellow=5,red=1,green=3,blue=9,test=1)
+probability2 = experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
+actual2 = probability2
+expected2 = 1.0
+
+print(probability2)
+
